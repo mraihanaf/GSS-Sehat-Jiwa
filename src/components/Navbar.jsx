442dd457a23ca0268a49252b708c24b9fdc5d81e
SalariedCrane47 <salariedc@gmail.com>
2024-08-18 16:30:31 +0700

    feat: add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <nav>
+            <img src="https://kss-uks.mardawa.id/wp-content/uploads/2023/05/Logo-Sekolah-Sehat-3-e1684443819880.png" alt="Logo Sekolah Sehat" />
+            <ul>
+                <li><a href="Home">Home</a></li>
+                <li><a href="About">About</a></li>
+                <li><a href="section 1">section 1</a></li>
+                <li><a href="section 2">section 2</a></li>
+                <li><a href="section 3">section 3</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
