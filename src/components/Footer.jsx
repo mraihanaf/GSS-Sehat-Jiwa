04fcede55c3c706394961724b2b73a0e6c5c82d9
Syarendra Hamzah Putra <shamzahputra@gmail.com>
2024-08-18 11:10:23 +0000

    feat: add footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,26 @@
 function Footer(){
-    const year = new Date().getFullYear()
+    const year = new Date().getFullYear();
     return(
-        <footer>
-            <img src="" alt="Sekolah-Sehat" />
-            <img src="" alt="Merdeka-Belajar" />
-            <div id="contact">
-                <h3>Contact</h3>
-                <p>Lokasi</p>
-                <p>Situs</p>
-                <p>E-mail</p>
-                <hr />
-
+        <footer className="bg-gray-800 text-white p-4">
+            <div className="flex items-center justify-between">
+                <img src="https://via.placeholder.com/200x100" alt="Sekolah-Sehat" />
+                <div className="flex-grow flex justify-center w-screen ">
+                    <div id="contact" className="text-start">
+                        <h3 className="text-3xl mb-2">Contact</h3>
+                        <ul className=''>
+                            <li><a href="#location">Lokasi</a></li>
+                            <li><a href="#website">Situs</a></li>
+                            <li><a href="mailto:example@example.com">E-Mail</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-
-            <ul>
-                <li><a href="#"></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-
-            </ul>
-            <hr />
-            <p>Copyright {year} - Penta-uno Developer</p>
+            <hr className="my-4 border-gray-600"/>
+            <div className="text-center">
+                <p>&copy; {year} PentaUno-Dev. All rights reserved.</p>
+            </div>
         </footer>
-    )
+    );
 }
 
 export default Footer;
